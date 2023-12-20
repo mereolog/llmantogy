20bf7cda52ee76bd70c8604b0aaebdc762b8c514
mereolog <pawel.garbacz@makolab.com>
2023-12-20 16:54:27 +0100

    mil verification code started
    Signed-off-by: mereolog <pawel.garbacz@makolab.com>

---
+++ agents/mil_verifier.py
@@ -0,0 +1,57 @@
+import json
+from tkinter import *
+
+
+accepted_texts = set()
+rejected_texts = set()
+text = str()
+main = None
+
+
+def __process_texts(english_text: str, clif_text: str):
+    global text
+    text = english_text
+    
+    global main
+    main = Tk()
+    main.title(english_text)
+    w = 1500
+    h = 750
+    ws = main.winfo_screenwidth()
+    hs = main.winfo_screenheight()
+    x = (ws / 2) - (w / 2)
+    y = (hs / 2) - (h / 2)
+    main.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    
+    clif_text_box = Text(main, height=5, width=52)
+    clif_text_box.insert(chars=clif_text, index=INSERT)
+    clif_text_box.pack(expand=True, fill=BOTH)
+    
+    accept_button = Button(main, text="Accept CLIF", command=__accept_button_action)
+    reject_button = Button(main, text="Reject CLIF", command=__reject_button_action)
+    accept_button.pack(side=LEFT)
+    reject_button.pack(side=RIGHT)
+    main.mainloop()
+
+
+def __accept_button_action():
+    accepted_texts.add(text)
+    main.destroy()
+    
+    
+def __reject_button_action():
+    rejected_texts.add(text)
+    main.destroy()
+
+
+def semantically_verify_consistent_translations(
+        clif_syntactically_verified_texts_file_path: str,
+        tptp_consistent_texts_file_path: str):
+    with open(file=clif_syntactically_verified_texts_file_path) as clif_syntactically_verified_texts_file:
+        clif_syntactically_verified_texts = json.load(fp=clif_syntactically_verified_texts_file)
+    with open(file=tptp_consistent_texts_file_path) as tptp_consistent_texts_file:
+        tptp_consistent_texts = json.load(fp=tptp_consistent_texts_file)
+    for english_text, clif_text in clif_syntactically_verified_texts.items():
+        if english_text in tptp_consistent_texts:
+            __process_texts(english_text=english_text, clif_text=clif_text)
+            
--- startup.py
+++ startup.py
@@ -1,4 +1,5 @@
 from agents.clif_verifier import verify_formalised_puzzles
+from agents.mil_verifier import semantically_verify_consistent_translations
 from agents.puzzle_formaliser import formalise_puzzles
 from agents.tptp_consistency_checker import check_puzzles_internal_consistency, check_puzzles_external_consistency
 
@@ -21,9 +22,13 @@ from agents.tptp_consistency_checker import check_puzzles_internal_consistency, 
 #     inconsistent_tptp_puzzles_file_path='outputs/internal_inconsistent_puzzles_tptp.json',
 #     undecided_tptp_puzzles_file_path='outputs/internal_undecided_puzzles_tptp.json')
 
-check_puzzles_external_consistency(
-    tptp_puzzles_file_path='outputs/internal_consistent_puzzles_tptp.json',
-    consistent_tptp_puzzles_file_path='outputs/dolce_consistent_puzzles_tptp.json',
-    inconsistent_tptp_puzzles_file_path='outputs/dolce_inconsistent_puzzles_tptp.json',
-    undecided_tptp_puzzles_file_path='outputs/dolce_undecided_puzzles_tptp.json',
-    external_theory_file_path='inputs/dolce.tptp')
+# check_puzzles_external_consistency(
+#     tptp_puzzles_file_path='outputs/internal_consistent_puzzles_tptp.json',
+#     consistent_tptp_puzzles_file_path='outputs/dolce_consistent_puzzles_tptp.json',
+#     inconsistent_tptp_puzzles_file_path='outputs/dolce_inconsistent_puzzles_tptp.json',
+#     undecided_tptp_puzzles_file_path='outputs/dolce_undecided_puzzles_tptp.json',
+#     external_theory_file_path='inputs/dolce.tptp')
+
+semantically_verify_consistent_translations(
+    clif_syntactically_verified_texts_file_path='midputs/verified_formalised_puzzles.json',
+    tptp_consistent_texts_file_path='outputs/dolce_consistent_puzzles_tptp.json')
